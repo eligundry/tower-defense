603ba9d39e90a39c461bd3dc2e30a2988f116094
Eli Gundry <eligundry@gmail.com>
2015-02-28 16:31:11 -0500

    Reorders models

--- game/models.py
+++ game/models.py
@@ -5,29 +5,6 @@ class GameManager(models.Manager):
         game = self.create(width=width, height=height)
         return game
 
-class Tile(models.Model):
-    x_coordinate = models.IntegerField()
-    y_coordinate = models.IntegerField()
-    is_edge = models.BooleanField(default=False)
-
-    def cell(self):
-        return (self.y_coordinate * 10) + self.x_coordinate
-
-class Tower(models.Model):
-    name = models.CharField(max_length=50)
-    class_name = models.CharField(max_length=50)
-    max_hp = models.IntegerField()
-    view_range = models.IntegerField(null=True)
-    damage = models.IntegerField(null=True)
-
-class Monster(models.Model):
-    name = models.CharField(max_length=50)
-    class_name = models.CharField(max_length=50)
-    max_hp = models.IntegerField()
-    attack_range = models.IntegerField(null=True)
-    movement_range = models.IntegerField()
-    damage = models.IntegerField()
-
 class Game(models.Model):
     money_earned = models.IntegerField(default=0)
     money_spent = models.IntegerField(default=0)
@@ -37,7 +14,7 @@ class Game(models.Model):
     height = models.IntegerField(default=100)
     time_start = models.DateTimeField(auto_now_add=True)
     time_end = models.DateTimeField(null=True)
-    tiles = models.ManyToManyField(Tile, through='GameTile')
+    tiles = models.ManyToManyField('Tile', through='GameTile')
 
     objects = GameManager()
 
@@ -55,23 +32,46 @@ class Game(models.Model):
                 game_tile = GameTile.objects.create(game=self, tile=tile)
 
 class GameTile(models.Model):
-    game = models.ForeignKey(Game)
-    tile = models.ForeignKey(Tile)
-    tower = models.ManyToManyField(Tower, through='TileTower',
+    game = models.ForeignKey('Game')
+    tile = models.ForeignKey('Tile')
+    tower = models.ManyToManyField('Tower', through='TileTower',
                                 through_fields=('tile', 'tower'), null=True)
-    monster = models.ManyToManyField(Monster, through='TileMonster',
+    monster = models.ManyToManyField('Monster', through='TileMonster',
                                 through_fields=('tile', 'monster'), null=True)
 
     def is_occupied(self):
         return (self.tower is not null) and (self.monster is not null)
 
-class TileTower(models.Model):
-    tile = models.ForeignKey(GameTile)
-    tower = models.ForeignKey(Tower)
-    hp = models.IntegerField()
-    view_range = models.IntegerField()
+class Tile(models.Model):
+    x_coordinate = models.IntegerField()
+    y_coordinate = models.IntegerField()
+    is_edge = models.BooleanField(default=False)
+
+    def cell(self):
+        return (self.y_coordinate * 10) + self.x_coordinate
+
+class Monster(models.Model):
+    name = models.CharField(max_length=50)
+    class_name = models.CharField(max_length=50)
+    max_hp = models.IntegerField()
+    attack_range = models.IntegerField(null=True)
+    movement_range = models.IntegerField()
+    damage = models.IntegerField()
 
 class TileMonster(models.Model):
-    tile = models.ForeignKey(GameTile)
-    monster = models.ForeignKey(Monster)
+    tile = models.ForeignKey('GameTile')
+    monster = models.ForeignKey('Monster')
     hp = models.IntegerField()
+
+class Tower(models.Model):
+    name = models.CharField(max_length=50)
+    class_name = models.CharField(max_length=50)
+    max_hp = models.IntegerField()
+    view_range = models.IntegerField(null=True)
+    damage = models.IntegerField(null=True)
+
+class TileTower(models.Model):
+    tile = models.ForeignKey('GameTile')
+    tower = models.ForeignKey('Tower')
+    hp = models.IntegerField()
+    view_range = models.IntegerField()
