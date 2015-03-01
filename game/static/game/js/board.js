dbd47fcaa65a5a5532a81e85ffa43e92f34b6462
Eli Gundry <eligundry@gmail.com>
2015-02-28 23:05:59 -0500

    Adds monsters to board on load

--- game/static/game/js/board.js
+++ game/static/game/js/board.js
@@ -3,6 +3,15 @@ function Board() {
 		tile: {
 			width: 100,
 			height: 100
+		},
+		monsters: {
+			width: 100,
+			height: 100,
+			icons: {
+				'archer': '/static/project_static/svg/Archer.svg',
+				'warrior': '/static/project_static/svg/Warrior.svg',
+				'brute': '/static/project_static/svg/Brute.svg'
+			}
 		}
 	};
 
@@ -12,16 +21,46 @@ function Board() {
 	this.svg_elm = $('#board');
 	this.width = this.data.width;
 	this.height = this.data.height;
+	this.monsters = this.data.monster;
+	this.monsters_group = [];
+	this.towers = this.data.towers;
 	this.tiles = [];
 
 	this.ResizeBoard(window.innerWidth, window.innerHeight);
 	this.GenerateBoard();
+	this.RenderMonsters(this.enemies);
 }
 
 Board.prototype.ResizeBoard = function(width, height) {
 	this.svg_elm.css({
 		height: height,
 		width: width
+	});
+};
+
+
+Board.prototype.RenderTowers = function() {
+	$.each(this.towers, function(i, e) {
+	});
+};
+
+Board.prototype.RenderMonsters = function() {
+	var self = this;
+
+	$.each(this.monsters, function(i, m) {
+		// this.group.image(image, position_x, position_y, size_x, size_y);
+		var target = $('[data-x="'+ m.x +'"][data-y="'+ m.y +'"]');
+		var x = Number(target.attr('x')),
+			y = Number(target.attr('y')),
+			icon = self.settings.monsters.icons[m['class']];
+
+		var monster = self.board_group.g().image(
+			icon,
+			x,
+			y,
+			self.settings.monsters.width,
+			self.settings.monsters.height
+		);
 	});
 };
 
@@ -40,9 +79,8 @@ Board.prototype.GenerateBoard = function() {
 				stroke: "#000",
 				strokeWidth: 5,
 				id: x + ", " + y,
-				class: "tileClass " + x + " " + y,
-				locationx: x,
-				locationy: y,
+				'data-x': x,
+				'data-y': y,
 				onmouseover: "evt.target.setAttribute('opacity', '0.5');",
 				onmouseout: "evt.target.setAttribute('opacity','1)');",
 				onclick: "tileMenu(this);"
